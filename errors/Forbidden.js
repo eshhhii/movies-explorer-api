7f600a596bd243dc83e89521991fa787d8ceab49
eshhhii <emmashiller@gmail.com>
2022-04-06 15:31:46 +0200

    ref: withiout cookies

---
+++ errors/Forbidden.js
@@ -0,0 +1,10 @@
+const { ERROR_CODE_403 } = require('../utils/constants');
+
+class Forbidden extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = ERROR_CODE_403;
+  }
+}
+
+module.exports = Forbidden;
---
+++ routes/movies.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const {
+  validationMovie,
+  validationMovieID,
+} = require('../middlewares/validation');
+const {
+  getMovies,
+  createMovie,
+  deleteMovie,
+} = require('../controllers/movies');
+
+router.get('/movies', getMovies);
+router.post('/movies', validationMovie, createMovie);
+router.delete('/movies/:movieId', validationMovieID, deleteMovie);
+
+module.exports = router;
