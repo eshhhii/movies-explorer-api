95537cc039de5c2d3059b8da7abe5d7f3e2e8207
eshhhii <emmashiller@gmail.com>
2022-05-29 12:36:06 +0200

    fix: убрать лайки и сохранения других пользователей возврат

--- controllers/movies.js
+++ controllers/movies.js
@@ -8,12 +8,9 @@ const {
   ERROR_MESSAGE_403,
 } = require('../utils/constants');
 
-const getMovies = (req, res, next) => {
-  const userId = req.user._id;
-  Movie.find({userId})
+const getMovies = (req, res, next) => Movie.find({owner: req.user._id})
   .then((movies) => res.status(200).send(movies))
   .catch(next);
-}
 
 const createMovie = (req, res, next) => {
   const {
